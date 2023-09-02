bb4bb66064a723628fe3b983c4c8cea64ba0fb60
codebytevilla <codebytevilla@gmail.com>
2023-09-02 17:47:33 +0530

    C program that accepts two strings str1 and str2, performs the specified operations based on their comparison, and displays the results

---
+++ F.Y.B.Sc. (Computer Science)/Advanced C Programming/Exercise 5/Other/string_compare.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// Function to reverse a string
+void reverseString(char str[]) {
+    int length = strlen(str);
+    int start = 0;
+    int end = length - 1;
+
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    char str1[100], str2[100];
+
+    // Accept input for str1 and str2
+    printf("Enter the first string (str1): ");
+    scanf("%s", str1);
+    printf("Enter the second string (str2): ");
+    scanf("%s", str2);
+
+    // Compare str1 and str2
+    int cmp = strcmp(str1, str2);
+
+    if (cmp == 0) {  // If str1 is equal to str2
+        // Display the length of str1 and str2
+        printf("Length of str1: %lu\n", strlen(str1));
+        printf("Length of str2: %lu\n", strlen(str2));
+    } else if (cmp < 0) {  // If str1 < str2
+        char str3[200];  // Create a larger buffer to store str1 + reversed str2
+
+        // Concatenate str1 and the reversed str2 into str3
+        strcpy(str3, str1);
+        strcat(str3, " ");
+        reverseString(str2);  // Reverse str2
+        strcat(str3, str2);
+
+        // Display str3
+        printf("Concatenated string (str1 + reversed str2): %s\n", str3);
+    } else {  // If str1 > str2
+        // Convert str1 to uppercase
+        for (int i = 0; str1[i]; i++) {
+            str1[i] = toupper(str1[i]);
+        }
+
+        // Convert str2 to lowercase
+        for (int i = 0; str2[i]; i++) {
+            str2[i] = tolower(str2[i]);
+        }
+
+        // Display the modified str1 and str2
+        printf("Uppercase str1: %s\n", str1);
+        printf("Lowercase str2: %s\n", str2);
+    }
+
+    return 0;
+}
