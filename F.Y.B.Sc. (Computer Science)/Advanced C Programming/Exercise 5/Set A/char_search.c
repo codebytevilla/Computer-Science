9cebd5c47a0ba26dd297173199efaca3ea559821
codebytevilla <codebytevilla@gmail.com>
2023-09-02 18:03:27 +0530

    C program that accepts a string and a character, then calls a function to search for the occurrence position of the character in the string and returns its position

---
+++ F.Y.B.Sc. (Computer Science)/Advanced C Programming/Exercise 5/Set A/char_search.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+
+// Function to search for a character in a string and return its position
+int searchCharacter(char str[], char ch)
+{
+    int length = strlen(str);
+
+    for (int i = 0; i < length; i++)
+    {
+        if (str[i] == ch)
+        {
+            return i; // Character found, return its position
+        }
+    }
+
+    return -1; // Character not found
+}
+
+int main()
+{
+    char inputString[100];
+    char searchChar;
+
+    // Input string from the user
+    printf("Enter a string: ");
+    scanf("%s", inputString);
+
+    // Input character to search
+    printf("Enter a character to search: ");
+    scanf(" %c", &searchChar); // Notice the space before %c to consume the newline character
+
+    // Call the search function and get the result
+    int position = searchCharacter(inputString, searchChar);
+
+    if (position != -1)
+    {
+        printf("The character '%c' was found at position %d in the string.\n", searchChar, position);
+    }
+    else
+    {
+        printf("The character '%c' was not found in the string.\n", searchChar);
+    }
+
+    return 0;
+}
