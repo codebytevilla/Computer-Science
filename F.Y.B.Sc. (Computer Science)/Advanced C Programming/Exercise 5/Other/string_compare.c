dd750a32f99f2a6825309fa8edd69ec309a825b6
codebytevilla <codebytevilla@gmail.com>
2023-09-02 18:10:14 +0530

    C program that accepts two strings str1 and str2, performs the specified operations based on their comparison, and displays the results

--- F.Y.B.Sc. (Computer Science)/Advanced C Programming/Exercise 5/Other/string_compare.c
+++ F.Y.B.Sc. (Computer Science)/Advanced C Programming/Exercise 5/Other/string_compare.c
@@ -7,9 +7,10 @@ void reverseString(char str[]) {
     int length = strlen(str);
     int start = 0;
     int end = length - 1;
+    char temp;
 
     while (start < end) {
-        char temp = str[start];
+        temp = str[start];
         str[start] = str[end];
         str[end] = temp;
         start++;
@@ -19,6 +20,9 @@ void reverseString(char str[]) {
 
 int main() {
     char str1[100], str2[100];
+    char str3[200];  // Create a larger buffer to store str1 + reversed str2
+    int cmp;
+    int i;
 
     // Accept input for str1 and str2
     printf("Enter the first string (str1): ");
@@ -27,15 +31,13 @@ int main() {
     scanf("%s", str2);
 
     // Compare str1 and str2
-    int cmp = strcmp(str1, str2);
+    cmp = strcmp(str1, str2);
 
     if (cmp == 0) {  // If str1 is equal to str2
         // Display the length of str1 and str2
         printf("Length of str1: %lu\n", strlen(str1));
         printf("Length of str2: %lu\n", strlen(str2));
     } else if (cmp < 0) {  // If str1 < str2
-        char str3[200];  // Create a larger buffer to store str1 + reversed str2
-
         // Concatenate str1 and the reversed str2 into str3
         strcpy(str3, str1);
         strcat(str3, " ");
@@ -46,12 +48,12 @@ int main() {
         printf("Concatenated string (str1 + reversed str2): %s\n", str3);
     } else {  // If str1 > str2
         // Convert str1 to uppercase
-        for (int i = 0; str1[i]; i++) {
+        for (i = 0; str1[i]; i++) {
             str1[i] = toupper(str1[i]);
         }
 
         // Convert str2 to lowercase
-        for (int i = 0; str2[i]; i++) {
+        for (i = 0; str2[i]; i++) {
             str2[i] = tolower(str2[i]);
         }
 
